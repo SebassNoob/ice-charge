d3680981cffb0f5e196124996ac4e84e75da7c65
SebassNoob <sebastian.ong@hotmail.com>
2024-05-10 20:07:36 +0800

    chore: update nextconfig

--- next.config.js
+++ next.config.js
@@ -1,4 +1,31 @@
 /** @type {import('next').NextConfig} */
-const nextConfig = {};
+const path = require("path");
+
+const aliases = {
+  "@": "./src",
+  "@components": "./src/components",
+  "@providers": "./src/providers",
+  "@utils": "./src/utils",
+};
+
+const nextConfig = {
+  reactStrictMode: true,
+  swcMinify: true,
+  // except for webpack, other parts are left as generated
+  webpack: (config, context) => {
+    config.watchOptions = {
+      poll: 1000,
+      aggregateTimeout: 300,
+    };
+    config.resolve.alias = {
+      ...config.resolve.alias,
+      ...Object.entries(aliases).reduce((acc, [key, value]) => {
+        acc[key] = path.resolve(value);
+        return acc;
+      }, {}),
+    };
+    return config;
+  },
+};
 
 module.exports = nextConfig;

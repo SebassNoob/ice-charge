29179e36d7ce3c912fb0f1a46f701a7a75858dec
SebassNoob <sebastian.ong@hotmail.com>
2024-05-11 21:46:08 +0800

    chore: unused component

--- src/components/AppSubmitButton/types.ts
+++
@@ -1,7 +0,0 @@
-import { type ButtonProps } from "@mantine/core";
-import { type ReactNode } from "react";
-
-export interface AppSubmitButtonProps {
-  buttonProps?: ButtonProps;
-  children?: ReactNode;
-}
